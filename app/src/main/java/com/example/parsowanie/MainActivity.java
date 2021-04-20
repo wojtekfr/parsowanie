d6f82d11f86af7c1661d560da39d6bf5f377df6d
Wojciech Frabinski <wojtef@gmail.com>
2021-04-20 19:48:57 +0200

    interface

--- app/src/main/java/com/example/parsowanie/MainActivity.java
+++ app/src/main/java/com/example/parsowanie/MainActivity.java
@@ -1,14 +1,31 @@
-package com.example.parsowanie;
+  package com.example.parsowanie;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.parsowanie.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 Toast.makeText(getApplicationContext(), "clicked", Toast.LENGTH_LONG).show();
+                 binding.feed.setText("lalala");
+                 binding.searchResults.setText(binding.searchPrase.getText());
+                 binding.invalidateAll();
+            } 
+        });
+        //  setContentView(R.layout.activity_main);
     }
 }
