c33af177df3b17c03bfe6ad25963c2d980f5f7f6
Wojciech Frabinski <wojtef@gmail.com>
2021-04-21 20:34:29 +0200

    api

--- app/src/main/java/com/example/parsowanie/MainActivity.java
+++ app/src/main/java/com/example/parsowanie/MainActivity.java
@@ -4,14 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.parsowanie.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONObject;
+
+  public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    RequestQueue queue;
+    //String url = "https://jsonplaceholder.typicode.com/todos";
+    String url = "https://cat-fact.herokuapp.com/facts";
+    String responseString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +34,22 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                  Toast.makeText(getApplicationContext(), "clicked", Toast.LENGTH_LONG).show();
-                 binding.feed.setText("lalala");
+                 binding.feed.setText(responseString);
                  binding.searchResults.setText(binding.searchPrase.getText());
                  binding.invalidateAll();
-            } 
+            //    queue.add(json)
+            }
         });
+
+        queue = MySingleton.getInstance(this.getApplicationContext()).getRequestQueue();
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET,
+                url, null, response -> responseString=response.toString(), new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            responseString = "FAIL";
+            }
+        });
+        queue.add(jsonArrayRequest);
 
     }
 }
